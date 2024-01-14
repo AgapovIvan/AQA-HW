6bea8561c62d59fdf4f8d22c752e3fc83790cc85
AgapovIvan <agapovivand33@gmail.com>
2024-01-14 12:17:26 +0300

    изменил 3 задание

--- HW6/task3.py
+++ HW6/task3.py
@@ -4,14 +4,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(20)
+driver.implicitly_wait(15)
 
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-WebDriverWait(driver, 4).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#image-container")))
+WebDriverWait(driver, 15).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "#landscape"))) 
 
 third_image_src = driver.find_element(By.CSS_SELECTOR, "#award").get_attribute("src")
 
 print(third_image_src)
 
 driver.quit()
+
